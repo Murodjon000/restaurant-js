04f7a18de211b434826e9c50b96808a11be89b72
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-08 16:17:06 +0500

    add tab events component

---
+++ src/components/tab.js
@@ -0,0 +1,24 @@
+const openTab = (id) => {
+  document.querySelectorAll('.menu-tab').forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  document.querySelectorAll('.menu-item').forEach(item => {
+    item.classList.remove('active');
+  });
+  
+  document.getElementById(id).classList.add('active');
+  document.querySelector(`div[data-id='${id}']`).classList.add('active');
+};
+
+const addTabEvent = () => {
+  document.querySelectorAll('.menu-tab').forEach(tab => {
+    tab.addEventListener('click', e =>{
+      openTab(e.target.getAttribute('data-id'));
+    });
+  });
+
+  document.getElementById('default-open').click();
+};
+
+export default addTabEvent;
