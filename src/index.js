a737b31ea6195311e479c1c3a4a5fc059c5576e5
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-03-08 16:19:58 +0500

    add all imports to index js file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './stylesheets/index.scss';
+import myHeader from './components/header';
+import mainPage from './components/main';
+import menuPage from './components/menu';
+import addTabEvent from './components/tab';
+import myFooter from './components/footer';
+
+myHeader();
+mainPage();
+menuPage();
+myFooter();
+addTabEvent();
